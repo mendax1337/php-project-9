b6e4ba591c86bcdc4d551b04241bbfa56fb02108
Sergey Martynov <sergey.martynov@dostavista.ru>
2025-08-05 12:50:05 +0300

    add main page template with Bootstrap

--- public/index.php
+++ public/index.php
@@ -3,12 +3,14 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 use Slim\Factory\AppFactory;
+use Slim\Views\PhpRenderer;
 
 $app = AppFactory::create();
 
-$app->get('/', function ($request, $response) {
-    $response->getBody()->write('Welcome to Page Analyzer!');
-    return $response;
+$renderer = new PhpRenderer(__DIR__ . '/../templates');
+
+$app->get('/', function ($request, $response) use ($renderer) {
+    return $renderer->render($response, 'main.phtml');
 });
 
 $app->run();
