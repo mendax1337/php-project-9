ff4f2ed16161aa07ea8b6b59ccb2b106c2cd472a
Sergey Martynov <sergey.martynov@dostavista.ru>
2025-08-08 20:46:04 +0300

    linter fix

--- src/helpers.php
+++ src/helpers.php
@@ -4,13 +4,22 @@ if (!function_exists('optional')) {
     function optional($value, $callback = null)
     {
         if (is_null($callback)) {
-            return new class($value) {
+            return new class($value)
+            {
                 private $value;
-                public function __construct($value) { $this->value = $value; }
-                public function __get($name) {
+
+                public function __construct($value)
+                {
+                    $this->value = $value;
+                }
+
+                public function __get($name)
+                {
                     return is_null($this->value) ? null : $this->value->$name;
                 }
-                public function __call($name, $arguments) {
+
+                public function __call($name, $arguments)
+                {
                     return is_null($this->value) ? null : $this->value->$name(...$arguments);
                 }
             };
